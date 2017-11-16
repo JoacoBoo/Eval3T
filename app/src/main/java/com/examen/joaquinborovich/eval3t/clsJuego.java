837e01180996e2b9167efaf895bb01a5b56c8098
Joaquin Borovich <joaquinborovich@hotmail.com>
2017-11-16 10:07:40 -0300

    listo

--- app/src/main/java/com/examen/joaquinborovich/eval3t/clsJuego.java
+++ app/src/main/java/com/examen/joaquinborovich/eval3t/clsJuego.java
@@ -1,13 +1,24 @@
 package com.examen.joaquinborovich.eval3t;
 
 import android.util.Log;
+import android.view.MotionEvent;
 
+import org.cocos2d.actions.interval.IntervalAction;
+import org.cocos2d.actions.interval.MoveBy;
+import org.cocos2d.actions.interval.MoveTo;
+import org.cocos2d.actions.interval.RotateBy;
+import org.cocos2d.actions.interval.RotateTo;
+import org.cocos2d.actions.interval.ScaleBy;
+import org.cocos2d.actions.interval.ScaleTo;
+import org.cocos2d.actions.interval.Sequence;
 import org.cocos2d.layers.Layer;
 import org.cocos2d.nodes.Director;
 import org.cocos2d.nodes.Scene;
 import org.cocos2d.nodes.Sprite;
 import org.cocos2d.opengl.CCGLSurfaceView;
 import org.cocos2d.types.CCSize;
+
+import java.util.Random;
 
 /**
  * Created by 42374778 on 16/11/2017.
@@ -27,9 +38,15 @@ public class clsJuego
     Sprite PersonaCuatro;
 
 
+    float MitadHeight;
+    float MitadWidth;
+    float CuartoWidth;
+    float CuartoHeight;
+    float TresCuartosHeight;
+    float TresCuartosWidth;
 
 
-    public clsJuego()
+    public clsJuego(CCGLSurfaceView vistaPrincipal)
     {
 
     }
@@ -49,18 +66,18 @@ public class clsJuego
         Log.d(TAG, "ComenzarJuego: Alto" + HeightDispositivo);
 
         Log.d(TAG, "ComenzarJuego: Calculo la mitad de la pantalla");
-        float MitadHeight = HeightDispositivo/2;
-        float MitadWidth = WidthDispositivo/2;
+        MitadHeight = HeightDispositivo/2;
+        MitadWidth = WidthDispositivo/2;
 
 
         Log.d(TAG, "ComenzarJuego: Calculo los cuartos de la pantalla");
-        float CuartoHeight = MitadHeight/2;
-        float CuartoWidth = MitadWidth/2;
+        CuartoHeight = MitadHeight/2;
+        CuartoWidth = MitadWidth/2;
 
 
         Log.d(TAG, "ComenzarJuego: Calculo 3 cuartos de la pantalla");
-        float TresCuartosHeight = CuartoHeight*3;
-        float TresCuartosWidth = CuartoWidth*3;
+        TresCuartosHeight = CuartoHeight*3;
+        TresCuartosWidth = CuartoWidth*3;
 
         Log.d(TAG, "ComenzarJuego: Le digo al director que ejecute la escena");
         Director.sharedDirector().runWithScene(EscenaDelJuego());
@@ -106,12 +123,94 @@ public class clsJuego
             PersonaTres = Sprite.sprite("cebo.jpg");
             PersonaCuatro = Sprite.sprite("lishu.jpg");
 
+            PersonaUno.setPosition(CuartoWidth, CuartoHeight);
+            PersonaDos.setPosition(CuartoWidth, TresCuartosHeight);
+            PersonaTres.setPosition(TresCuartosWidth, CuartoHeight);
+            PersonaCuatro.setPosition(TresCuartosWidth, TresCuartosHeight);
+
             super.addChild(PersonaUno);
             super.addChild(PersonaDos);
             super.addChild(PersonaTres);
             super.addChild(PersonaCuatro);
         }
 
+        @Override
+        public boolean ccTouchesBegan(MotionEvent event)
+        {
+            Random RandomNum = new Random();
+            int azar = RandomNum.nextInt(3);
+
+            switch (azar)
+            {
+                case 0:
+                    PersonaUno.runAction(RotateBy.action(1f, 360f));
+                    PersonaDos.runAction(RotateBy.action(1f, -360f));
+                    PersonaTres.runAction(RotateBy.action(1f, -360f));
+                    PersonaCuatro.runAction(RotateBy.action(1f, -360f));
+
+                    break;
+                case 1:
+                    PersonaUno.runAction(RotateBy.action(1f, -360f));
+                    PersonaDos.runAction(RotateBy.action(1f, 360f));
+                    PersonaTres.runAction(RotateBy.action(1f, -360f));
+                    PersonaCuatro.runAction(RotateBy.action(1f, -360f));
+
+                    break;
+                case 2:
+                    PersonaUno.runAction(RotateBy.action(1f, -360f));
+                    PersonaDos.runAction(RotateBy.action(1f, -360f));
+                    PersonaTres.runAction(RotateBy.action(1f, 360f));
+                    PersonaCuatro.runAction(RotateBy.action(1f, -360f));
+
+                    break;
+                case 3:
+                    PersonaUno.runAction(RotateBy.action(1f, -360f));
+                    PersonaDos.runAction(RotateBy.action(1f, -360f));
+                    PersonaTres.runAction(RotateBy.action(1f, -360f));
+                    PersonaCuatro.runAction(RotateBy.action(1f, 360f));
+
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+
+        @Override
+        public boolean ccTouchesMoved(MotionEvent event)
+        {
+            if (PersonaUno.getPositionX()>(0f+100f))
+            {
+                PersonaUno.runAction(MoveTo.action(0.1f, PersonaUno.getPositionX()-30f, PersonaUno.getPositionY()));
+            }
+            if (PersonaDos.getPositionX()>(0f+100f))
+            {
+                PersonaDos.runAction(MoveTo.action(0.1f, PersonaDos.getPositionX()-30f, PersonaDos.getPositionY()));
+            }
+            if (PersonaTres.getPositionX()<=(WidthDispositivo-100f))
+            {
+                PersonaTres.runAction(MoveTo.action(0.1f, PersonaTres.getPositionX()+30f, PersonaTres.getPositionY()));
+            }
+            if (PersonaCuatro.getPositionX()<=(WidthDispositivo-100f))
+            {
+                PersonaCuatro.runAction(MoveTo.action(0.1f, PersonaCuatro.getPositionX()+30f, PersonaCuatro.getPositionY()));
+            }
+
+            return true;
+        }
+
+        @Override
+        public boolean ccTouchesEnded(MotionEvent event)
+        {
+            ScaleBy grande;
+            ScaleTo normal;
+            grande = ScaleBy.action(0.7f, 1.5f, 1.5f);
+            //normal = ScaleBy.action(0.1f , -1f, -1f);
+            normal = ScaleTo.action(0.5f , 1f, 1f);
+            IntervalAction Secuencia = Sequence.actions(grande, normal, grande, normal, grande, normal);
+            PersonaUno.runAction(Secuencia);
+            return true;
+        }
     }
 
 }
